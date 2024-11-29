312c436c95e7548b21cc317976c8d5e08583fc1e
bohara-s <sushilbohara826@gmail.com>
2024-11-29 17:11:37 +0545

    variables

--- day2 variable.py
+++ day2 variable.py
@@ -1,3 +1,4 @@
+"""
 x=1,2,3,4,5,6,7,8,9,10
 print(x)
 print(1)
@@ -10,3 +11,29 @@ print(7)
 print(8)
 print(9)
 print(10)
+x="1234"#str
+y="bohara"#str
+z=123#int
+print(x,y,z)
+print( "type of x",type(x))
+print("type of y",type(y))
+print("type of z",type(z))
+
+age=22
+salery=2.2
+name= "bohara"
+print(type(age))
+print(type(salery))
+print(type(name))
+
+#x=int(name)
+#y=float(age)
+#z=int(salery)
+
+
+"""
+salery=12.33
+print(type(salery))
+salery=int(12.33)
+print(salery)
+print(type(salery))
